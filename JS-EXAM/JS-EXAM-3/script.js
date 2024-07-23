ca31ad5f0ace61efdf8ca903e72b830c6bd63cbd
daksh-kunjadiay <dakshkunjadiya206@gmail.com>
2024-07-23 11:55:53 +0530

    23/07/2024

--- JS-EXAM/JS-EXAM-3/script.js
+++ JS-EXAM/JS-EXAM-3/script.js
@@ -1,37 +1,57 @@
-let students = [];
+let users = [];
 
-let uimaker = () => {
-    document.getElementById('tbody').innerHTML = "";
-    for (let i = 0; i < students.length; i++) {
-        let tr = document.createElement('tr');
-        let td1 = document.createElement('td');
-        td1.innerHTML = students[i].name;
-        let td2 = document.createElement('td');
-        td2.innerHTML = students[i].grid;
-        let td3 = document.createElement('td');
-        td3.innerHTML = students[i].course;
-        let td4 = document.createElement('td');
-        td4.innerHTML = students[i].number;
-        let td5 = document.createElement('td');
-        td5.innerHTML = students[i].fee;
-        tr.append(td1, td2, td3, td4, td5);
-        document.getElementById('tbody').append(tr);
-    }
-}
-
-const handledata = (e) => {
-    e.preventDefault();
-    let data = {
-        name: document.getElementById("name").value,
-        grid: document.getElementById("grid").value,
-        course: document.getElementById("course").value,
-        number: document.getElementById("number").value,
-        fee: document.getElementById("fee").value
-    }
-    students.push(data);
+const handleDelete = (index) => {
+    users.splice(index, 1);
     uimaker();
+};
 
+const uimaker = () => {
+    document.getElementById("tbody").innerHTML = "";
+    for (let i = 0; i < users.length; i++) {
+        let tr = document.createElement("tr");
+        let td1 = document.createElement("td");
+        td1.innerHTML = users[i].username;
+        let td2 = document.createElement("td");
+        td2.innerHTML = users[i].grid;
+        let td3 = document.createElement("td");
+        td3.innerHTML = users[i].course;
+        let td4 = document.createElement("td");
+        td4.innerHTML = users[i].fees;
+        let td5 = document.createElement("td");
+        td5.innerHTML = users[i].number;
+        let td6 = document.createElement("td");
+        td6.innerHTML = "delete";
+        td6.addEventListener("click", handleDelete);
+        document.getElementById("tbody").append(tr);
+        tr.append(td1, td2, td3, td4, td5, td6);
+    }
+};
 
-}
+const handleData = (e) => {
+    e.preventDefault();
 
-document.getElementById("userData").addEventListener("submit", handledata);
+    let user = {
+        username: document.getElementById("Name").value.trim(),
+        grid: document.getElementById("grid").value.trim(),
+        course: document.getElementById("course").value.trim(),
+        fees: document.getElementById("fees").value.trim(),
+        number: document.getElementById("number").value.trim(),
+    };
+    if (user.grid.length != 4) {
+        alert("GRID should be 4 digits long");
+        return;
+    }
+    if (
+        user.number.length != 10 ||
+        !["6", "7", "8", "9"].includes(user.number[0])
+    ) {
+        alert(
+            "Phone number should be at least 10 digits long and start with 6,7,8,9"
+        );
+        return;
+    }
+    users.push(user);
+    uimaker();
+};
+
+document.getElementById("UserData").addEventListener("submit", handleData);
