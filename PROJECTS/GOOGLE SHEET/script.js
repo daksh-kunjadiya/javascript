5712fb5bc8e135f5332f3a7bf52960d55d0d852f
daksh-kunjadiay <dakshkunjadiya206@gmail.com>
2024-10-04 15:29:01 +0530

    04/10/2024

--- PROJECTS/GOOGLE SHEET/script.js
+++ PROJECTS/GOOGLE SHEET/script.js
@@ -1,4 +1,4 @@
-let url = "https://script.google.com/macros/s/AKfycbyHfH-jA_wePmnAbLqoCMJzp8CoOEi8ZBuT5N1iOtJwFI7r_whFTqipJ2Xz5W9mFi6LkQ/exec"
+let url = "https://script.google.com/macros/s/AKfycbz5zTXPsX2SFCuR-1H4lUEKuBlRlEGNSRtXLeGQYYDKyhrtA9u_kkCSrbDb55_gqqRczg/exec"
 let form = document.querySelector("#UserData");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
