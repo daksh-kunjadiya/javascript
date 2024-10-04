184c10471a5f8c0d4997a52cdd0038c2c8795cfb
daksh-kunjadiay <dakshkunjadiya206@gmail.com>
2024-10-04 15:41:05 +0530

    04/10/2024

--- PROJECTS/GOOGLE SHEET/script.js
+++ PROJECTS/GOOGLE SHEET/script.js
@@ -1,12 +1,22 @@
-let url = "https://script.google.com/macros/s/AKfycbz5zTXPsX2SFCuR-1H4lUEKuBlRlEGNSRtXLeGQYYDKyhrtA9u_kkCSrbDb55_gqqRczg/exec"
+let url = "https://script.google.com/macros/s/AKfycbz5zTXPsX2SFCuR-1H4lUEKuBlRlEGNSRtXLeGQYYDKyhrtA9u_kkCSrbDb55_gqqRczg/exec";
 let form = document.querySelector("#UserData");
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    let Data = new FormData(form);
+    let formData = new FormData(form);
+    let params = new URLSearchParams();
+    formData.forEach((value, key) => {
+        params.append(key, value);
+    });
     fetch(url, {
         method: "POST",
-        body: Data
-    }).then((res) => res.text())
-        .then((finaLRes) => console.log(finaLRes))
-    alert("DATA SAVED")
-})
+        body: params
+    }).then(response => response.text())
+        .then(result => {
+            console.log(result);
+            alert("Data Saved");
+        }).catch(error => {
+            console.error("Error:", error);
+            alert("Error Saving Data");
+        });
+});
